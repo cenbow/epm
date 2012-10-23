ec202e924e8e3e3c8a8e2ae196228849d8c3666b
Lourival Sabino <lourival.junior@tse.jus.br>
2012-10-23 14:44:42 -0200

    Problema na assinatura no Windows;

--- epm-signer/src/main/java/br/net/woodstock/epm/signer/common/WindowsMYStoreTypeHandler.java
+++ epm-signer/src/main/java/br/net/woodstock/epm/signer/common/WindowsMYStoreTypeHandler.java
@@ -1,6 +1,5 @@
 package br.net.woodstock.epm.signer.common;
 
-import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 
 import javax.swing.JOptionPane;
@@ -24,10 +23,11 @@ public class WindowsMYStoreTypeHandler implements StoreTypeHandler {
 	public void execute() {
 		try {
 			KeyStore keyStore = KeyStore.getInstance(KeyStoreType.WINDOWS_MY.getType(), ProviderType.SUN_MSCAPI.getType());
+			keyStore.load(null, null);
 			this.store = new JCAStore(keyStore);
 			SignerHolder.getInstance().setStore(this.store);
 			SignerHolder.getInstance().onSelectStore();
-		} catch (GeneralSecurityException e) {
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e, SignerHolder.getInstance().getMessage().getMessage(Constants.LABEL_ERROR), JOptionPane.ERROR_MESSAGE);
 			SignerLog.getLogger().debug(e.getMessage(), e);
 		}
