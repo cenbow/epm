79ea8d2e65e05712b1e8bcdcdde6090cf54a1910
Lourival Sabino <lourival.sabino.junior@gmail.com>
2012-11-10 16:00:05 -0200

    testes do datatable

--- epm-web/src/main/java/br/net/woodstock/epm/web/AbstractSearch.java
+++ epm-web/src/main/java/br/net/woodstock/epm/web/AbstractSearch.java
@@ -6,4 +6,24 @@ public abstract class AbstractSearch implements Serializable {
 
 	private static final long	serialVersionUID	= -4313009035332736312L;
 
+	private String				selectedIndex;
+
+	public String getSelectedIndex() {
+		return this.selectedIndex;
+	}
+
+	public void setSelectedIndex(String selectedIndex) {
+		this.selectedIndex = selectedIndex;
+	}
+
+	// Aux
+	public int getFirstRow() {
+		if (this.getSelectedIndex() == null) {
+			return 0;
+		}
+		//int selectedIndex = this.getSelectedIndex().intValue();
+		//return selectedIndex;
+		return 0;
+	}
+
 }
